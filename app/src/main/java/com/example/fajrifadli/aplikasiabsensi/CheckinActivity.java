2307929e3190b465d525b2ec8e120c2cad08e6f4
Fajri Fadli <fajri.fadli104@gmail.com>
2018-10-18 08:51:53 +0700

    Add button listener in CheckinActivity

--- app/src/main/java/com/example/fajrifadli/aplikasiabsensi/CheckinActivity.java
+++ app/src/main/java/com/example/fajrifadli/aplikasiabsensi/CheckinActivity.java
@@ -3,11 +3,22 @@ package com.example.fajrifadli.aplikasiabsensi;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class CheckinActivity extends AppCompatActivity {
+    float radius = 50;
+    Button buttonAbsen;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkin);
+        buttonAbsen = findViewById(R.id.buttonAbsen);
+        buttonAbsen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
