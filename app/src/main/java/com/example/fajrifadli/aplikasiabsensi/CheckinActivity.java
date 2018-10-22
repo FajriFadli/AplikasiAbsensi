6a6ee702457df439bdac9d4c98a243f0e45d3e59
Fajri Fadli <fajri.fadli104@gmail.com>
2018-10-22 20:18:20 +0700

    Allow users to check in with their name & the time they check in

--- app/src/main/java/com/example/fajrifadli/aplikasiabsensi/CheckinActivity.java
+++ app/src/main/java/com/example/fajrifadli/aplikasiabsensi/CheckinActivity.java
@@ -9,6 +9,8 @@ import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -18,17 +20,23 @@ public class CheckinActivity extends AppCompatActivity {
     Button buttonAbsen;
     GoogleSignInAccount account;
     String name;
-    SimpleDateFormat simpledateformat;
+    SimpleDateFormat hour;
+    SimpleDateFormat month;
     Calendar calendar;
-    String date;
+    String timeHour;
+    String timeMonth;
+    DatabaseReference mDatabase;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkin);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
         account = GoogleSignIn.getLastSignedInAccount(this);
         calendar = Calendar.getInstance();
-        simpledateformat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-        date = simpledateformat.format(calendar.getTime());
+        hour = new SimpleDateFormat("HH:mm:ss");
+        month = new SimpleDateFormat("dd-MM-yyyy");
+        timeHour = hour.format(calendar.getTime());
+        timeMonth = month.format((calendar.getTime()));
         if(account != null){
             name = account.getDisplayName();
         }
@@ -36,7 +44,8 @@ public class CheckinActivity extends AppCompatActivity {
         buttonAbsen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), name + " " + date, Toast.LENGTH_LONG).show();
+                mDatabase.child(name).child(timeMonth).child("Waktu Datang").setValue(timeHour);
+                Toast.makeText(getApplicationContext(), "Absen Sukses", Toast.LENGTH_LONG).show();
             }
         });
     }
