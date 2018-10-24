f037d0bc875ade61ad430526347db0eaad34cf4c
Fajri Fadli <fajri.fadli104@gmail.com>
2018-10-24 12:40:08 +0700

    Add raw data to Admin Panel

--- app/src/main/java/com/example/fajrifadli/aplikasiabsensi/AdminPanelActivity.java
+++ app/src/main/java/com/example/fajrifadli/aplikasiabsensi/AdminPanelActivity.java
@@ -1,13 +1,59 @@
 package com.example.fajrifadli.aplikasiabsensi;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class AdminPanelActivity extends AppCompatActivity {
+    DatabaseReference mDatabase;
+    ListView mListView;
+    ArrayList<String> WaktuAbsen = new ArrayList<>();
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_panel);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mListView = findViewById(R.id.list_absen);
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, WaktuAbsen);
+        mListView.setAdapter(arrayAdapter);
+        mDatabase.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                String value = dataSnapshot.toString();
+                WaktuAbsen.add(value);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
